69c949397d3d48a32940cbeebda9e9cb9662bd5b
Hualdo <hucastillo@gmail.com>
2022-02-28 11:42:08 -0500

    Toggle functionality

--- pricing.js
+++ pricing.js
@@ -5,9 +5,58 @@ let frecuency
 let planType = document.getElementById('planType')
 let left
 
-document.getElementById('range').addEventListener('input',calculate)
+document.getElementById('range').addEventListener('change',calculate)
+document.getElementById('toggle').addEventListener('click',updateToggle)
 
+function updateToggle(){
+    document.getElementById('toggle').classList.toggle('toggleActive')
+    price=document.getElementById('price').textContent
+    range=document.getElementById('range').value
+    frecuency=document.getElementById('frecuency')
+    if(planType.checked){
+        frecuency.textContent='/ month'
+        
+        if(range==1){
+            price=72/12/0.75
+            document.getElementById('price').textContent=price            
+        }else if(range==2){
+            price=108/12/0.75
+            document.getElementById('price').textContent=price            
+        }else if(range==3){
+            price=144/12/0.75
+            document.getElementById('price').textContent=price
+        }else if(range==4){
+            price=216/12/0.75
+            document.getElementById('price').textContent=price
+        }else {
+            price=324/12/0.75
+            document.getElementById('price').textContent=price
+        }     
+       
+    }
 
+    else{
+        frecuency.textContent='/ year'
+        if(range==1){
+            price=8*12*0.75
+            document.getElementById('price').textContent=price
+        }else if(range==2){
+            price=12*12*0.75
+            document.getElementById('price').textContent=price
+        }else if(range==3){
+            price=16*12*0.75
+            document.getElementById('price').textContent=price
+        }else if(range==4){
+            price=24*12*0.75
+            document.getElementById('price').textContent=price
+        }else{
+            price=36*12*0.75
+            document.getElementById('price').textContent=price
+        }
+            
+    }
+    
+}
 
 function calculate(){
     range = document.getElementById('range')
@@ -37,14 +86,13 @@ function calculate(){
             document.getElementById('price').textContent='$'+price 
             pageViews.textContent='50k pageviews'
             frecuency='/ year'
-            document.getElementById('frecuency').textContent=frecuency           
+            document.getElementById('frecuency').textContent=frecuency          
         }else
         pageViews.textContent='50k pageviews'
         left=25
         price.textContent='$12'
         document.getElementById('range').style.setProperty('--left',left)
-        frecuency='/ month'
-        document.getElementById('frecuency').textContent=frecuency
+        frecuency.textContent='/ month'        
     }
     else if(range.value==3){
         if(planType.checked){
@@ -90,4 +138,3 @@ function calculate(){
         frecuency.textContent='/ month'
     }
 }
-
